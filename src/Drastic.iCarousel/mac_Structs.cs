857911c2db6b99735e571bbf0d5c1ce8c05d6a01
Timothy Miller <innerlogic4321@gmail.com>
2023-01-04 19:59:50 +0900

    Initial

---
+++ src/Drastic.iCarousel/mac_Structs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using ObjCRuntime;
+
+namespace Drastic.iCarousel
+{
+	[Native]
+	public enum iCarouselType : long
+	{
+		Linear = 0,
+		Rotary,
+		InvertedRotary,
+		Cylinder,
+		InvertedCylinder,
+		Wheel,
+		InvertedWheel,
+		CoverFlow,
+		CoverFlow2,
+		TimeMachine,
+		InvertedTimeMachine,
+		Custom
+	}
+
+	[Native]
+	public enum iCarouselOption : long
+	{
+		Wrap = 0,
+		ShowBackfaces,
+		OffsetMultiplier,
+		VisibleItems,
+		Count,
+		Arc,
+		Angle,
+		Radius,
+		Tilt,
+		Spacing,
+		FadeMin,
+		FadeMax,
+		FadeRange,
+		FadeMinAlpha
+	}
+}
